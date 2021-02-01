0bcbe19f9cb27c325ee0d6fc21d3d1e2389c53ab
DarrylBonnnicib43840 <56337637+DarrylBonnnicib43840@users.noreply.github.com>
2021-02-01 14:57:46 +0100

    Scripts

---
+++ Scripts/Ku2Obsticle.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ku2Obsticle : MonoBehaviour
+{
+    [SerializeField]
+    GameObject obstaclePrefab;
+    public List<Vector3> freeObstacles  = new List<Vector3>();
+    private GameObject obstacleParent;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obstacleParent = new GameObject("Obsticle");
+        obstacleParent.transform.position = new Vector3(0,0);
+
+        for(int i = 1;i < 6;i++)
+        {
+            AddObstacle();
+
+            if(i==5)
+            {
+                
+                Scan();
+                StartAI();
+            }
+        }
+    }
+	
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartAI()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.GetComponent<customAiMoveScript>().enabled = true;
+        }
+    }
+   
+    public Vector3 RandomPosition()
+    {
+        Vector3 pos;
+        int xnode;
+        int ynode;
+
+        do
+        {
+            xnode = Random.Range(-49, 49);
+            ynode = Random.Range(-49, 49);
+            pos = new Vector3(xnode,ynode);
+            
+        }
+        while(freeObstacles.Contains(new Vector3(xnode,ynode)));
+
+        return pos;
+    }
+	
+	public void AddObstacle()
+    {
+        Vector3 pos = RandomPosition();
+
+        freeObstacles.Add(pos);
+
+        GameObject obstacle = Instantiate(obstaclePrefab,pos,Quaternion.identity);
+
+        obstacle.transform.SetParent(obstacleParent.transform);
+    }
+	public void Scan()
+    {
+        GameObject.Find("AStarGrid").GetComponent<AstarPath>().Scan();
+    }
+}
+
---
+++ Scripts/manager.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class manager : MonoBehaviour
+{
+	
+	
+	[SerializeField]
+    GameObject obsticle;
+	
+    public List<Vector3> freeObsticles  = new List<Vector3>();
+
+    [SerializeField]
+    GameObject EnemyAI;
+
+    private GameObject obsticleParent;
+    private GameObject enemyParent;
+	
+	
+    // Start is called before the first frame update
+    void Start()
+    {
+		obsticleParent = new GameObject("Obsticles");
+        obsticleParent.transform.position = new Vector3(0,0);
+
+        enemyParent = new GameObject("Enemies");
+        enemyParent.transform.position = new Vector3(0,0);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+	public Vector3 RandomPos()
+    {
+        Vector3 pos;
+        int xnode;
+        int ynode;
+
+        do
+        {
+            xnode = Random.Range(-49, 49);
+            ynode = Random.Range(-49, 49);
+            pos = new Vector3(xnode,ynode);
+           
+        }
+        while(freeObsticles.Contains(new Vector3(xnode,ynode)));
+
+        return pos;
+    }
+	
+	 public void Scan()
+    {
+        GameObject.Find("AStarGrid").GetComponent<AstarPath>().Scan();
+    }
+	
+	public void AddAI()
+    {
+        GameObject enemy = Instantiate(EnemyAI,RandomPos(),Quaternion.identity);
+
+        enemy.transform.SetParent(enemyParent.transform);
+    }
+	
+	public void AddObsticles()
+    {
+        Vector3 pos = RandomPos();
+
+        freeObsticles.Add(pos);
+
+        GameObject obsticle2 = Instantiate(obsticle,pos,Quaternion.identity);
+        obsticle.transform.SetParent(obsticleParent.transform);
+    }
+	
+	public void startAI()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+
+        foreach (GameObject enemy in enemies)
+        {
+            enemy.GetComponent<customAiMoveScript>().enabled = true;
+        }
+    }
+}
